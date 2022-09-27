4ff21b8cadb7f4f54f3f76ab59ca56efa9a14b33
Fahad123k <ahmad.nishu08@gmail.com>
2022-09-27 21:23:13 +0530

    add search list AND load deatil funct at console

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,72 @@
+// Examples 
 // http://www.omdbapi.com/?4a7dc175=[yourkey]&
 // http://www.omdbapi.com/?4a7dc175=[avengers]&
 // http://www.omdbapi.com/?t=thor&apikey=4a7dc175
+
+// https://omdbapi.com/?apikey=4a7dc175&s=avengers&type=movie&page=1
+// https://omdbapi.com/?apikey=4a7dc175&s=avengers&page=1
+// const URL=`https://omdbapi.com/?apikey=4a7dc175&s=${searchTerm}&page=1`;
+// example end
+
+// fetching data 
+const movieSearchBox = document.getElementById('movie-search-box');
+const searchList = document.getElementById('search-list');
+const resultGrid = document.getElementById('result-grid');
+
+
+// load movies from API
+async function loadMovies(searchTerm){
+    const URL = `https://omdbapi.com/?s=${searchTerm}&page=1&apikey=4a7dc175`;
+    const res = await fetch(`${URL}`);
+    const data = await res.json();
+    // console.log(data.Search);
+    if(data.Response == "True") displayMovieList(data.Search);
+}
+
+
+function findMovies(){
+    let searchTerm = (movieSearchBox.value).trim();
+    if(searchTerm.length > 0){
+        searchList.classList.remove('hide-search-list');
+        loadMovies(searchTerm);
+    } else {
+        searchList.classList.add('hide-search-list');
+    }
+}
+
+
+
+function displayMovieList(movies){
+    searchList.innerHTML = "";
+    for(let idx = 0; idx < movies.length; idx++){
+        let movieListItem = document.createElement('div');
+        movieListItem.dataset.id = movies[idx].imdbID; // setting movie id in  data-id
+        movieListItem.classList.add('search-list-item');
+        if(movies[idx].Poster != "N/A")
+            moviePoster = movies[idx].Poster;
+        else 
+            moviePoster = "image_not_found.png";
+
+        movieListItem.innerHTML = `
+        <div class = "search-item-thumbnail">
+            <img src = "${moviePoster}">
+        </div>
+        <div class = "search-item-info">
+            <h3>${movies[idx].Title}</h3>
+            <p>${movies[idx].Year}</p>
+    
+        </div>
+        `;
+        searchList.appendChild(movieListItem);
+    }
+    loadMovieDetails();
+}
+
+function loadMovieDetails(){
+    const searchListMovies=searchList.querySelectorAll('search-list-item');
+    searchListMovies.forEach(movie=>{
+        movie.addEventListener('click',async()=>{
+            console.log(movie.dataset.id);
+        })
+    })
+}
